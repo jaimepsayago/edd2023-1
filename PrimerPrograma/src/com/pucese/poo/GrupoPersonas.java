036c71d5c228eaa3f61567910465d0f05ed1fe1a
Jaime Sayago Heredia <jaimepsayago@gmail.com>
2023-07-12 08:58:34 -0500

    polimorfismo poo

---
+++ PrimerPrograma/src/com/pucese/poo/GrupoPersonas.java
@@ -0,0 +1,34 @@
+package com.pucese.poo;
+
+import java.util.ArrayList;
+
+public class GrupoPersonas {
+	
+	//operaciones con datos para agregar o eliminar
+	
+	//delcara un arraylista de tipo persona
+	private ArrayList<Persona> listaPersonas;
+	
+	//inicializar nuestro arraylist
+	public GrupoPersonas() {
+			listaPersonas = new ArrayList<Persona>();
+	}
+	//anadir persona al arrayList
+	public void anadePersona(Persona persona) {
+		listaPersonas.add(persona);
+	}
+	
+
+	public eliminaPersona() {
+		
+	}
+	
+	public eliminaPersona() {
+		
+	}
+
+
+	
+	
+
+}
--- PrimerPrograma/src/com/pucese/poo/Persona.java
+++ PrimerPrograma/src/com/pucese/poo/Persona.java
@@ -1,30 +1,86 @@
 package com.pucese.poo;
 
+import java.util.Objects;
+
 public class Persona {
 	//propiedades
+	public String dni; 
 	public String nombre;
 	private int contrasenia;
 	protected String direccion;
 	
-	//abstraccion
-	//polimorfirmo
-	//herencia
 	
 	public Persona() {
 		
 	}
 	
-	//encapsulacion
-	//metodos
-	
-	
+	public Persona(String dni, String nombre, int contrasenia, String direccion) {
+		this.dni=dni;
+		this.nombre=nombre;
+		this.contrasenia=contrasenia;
+		this.direccion=direccion;
+	}
+	//getters and setters
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
-		this.nombre=nombre;
+		this.nombre = nombre;
 	}
+
+	public int getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(int contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	//metodos equals and toString
+	@Override
+	public int hashCode() {
+		return Objects.hash(contrasenia, direccion, dni, nombre);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return contrasenia == other.contrasenia && Objects.equals(direccion, other.direccion)
+				&& Objects.equals(dni, other.dni) && Objects.equals(nombre, other.nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", contrasenia=" + contrasenia + ", direccion="
+				+ direccion + "]";
+	}
+
+	
+	
+
+	
 	
 	
 	
